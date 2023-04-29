7ecbe886f0c4a6e5f22cfab1920e61c592d9af7d
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-04-28 18:10:00 -0700

    betu

--- free.c
+++ free.c
@@ -1,12 +1,18 @@
 #include "main.h"
+/**
+*free_tokens - frees the memory allocated for an array of tokens
+*@tokens: the array of tokens to free
+*Return: void
+*/
 void free_tokens(char **tokens)
 {
-    int i = 0;
-    while (tokens[i] != NULL)
-    {
-        free(tokens[i]); /* free memory allocated for each token */
-        i++;
-    }
-    free(tokens); /* free memory allocated for the tokens array */
+int i = 0;
+
+	while (tokens[i] != NULL)
+	{
+		free(tokens[i]); /* free memory allocated for each token */
+		i++;
+	}
+	free(tokens); /* free memory allocated for the tokens array */
 }
 
